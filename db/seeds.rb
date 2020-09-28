9ad9b5f1c84c71e1868a93d499a92cdd072883d4
Amandyne Verdonck <amandyne.verdonck@gmail.com>
2020-09-28 18:29:22 +0200

    skills

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
+puts"destroy skill"
+Skill.destroy_all
+puts "create skills"
+
+ruby =Skill.create!(name: "Ruby")
+css =Skill.create!(name: "CSS(3)")
+html =Skill.create!(name: "HTML(5)")
+bootstrap =Skill.create!(name: "Bootstrap")
+sql =Skill.create!(name: "SQL")
+command =Skill.create!(name: "Command line interface")
+js =Skill.create!(name: "JavaScript(ES6)")
+github =Skill.create!(name: "Github")
+heroku =Skill.create!(name: "Heroku")
+mvc =Skill.create!(name: "MVC pattern")
+rails =Skill.create!(name: "Ruby On Rails")
+wordpress =Skill.create!(name: "Wordpress")
+
+mac =Skill.create!(name: "Macintosh")
+figma =Skill.create!(name: "Figma")
+indesign =Skill.create!(name: "Adobe Indesign")
+photoshop =Skill.create!(name: "Adobe Photoshop")
+microsoft = Skill.create!(name: "Microsoft Office")
+iwork = Skill.create!(name: "iWork")
+
