16227739bc3a5592454aca05f7fd043a123965c3
Amandyne Verdonck <amandyne.verdonck@gmail.com>
2020-09-29 21:07:26 +0200

    behavior smooth

--- app/javascript/components/link.js
+++ app/javascript/components/link.js
@@ -6,7 +6,7 @@ let laptop = document.querySelector('.fa-laptop')
 let femaleClick = () => {
  female.addEventListener('click', (event)=>{
   console.log(event)
-  document.getElementById('profile-typed-text').scrollIntoView()
+  document.getElementById('profile-typed-text').scrollIntoView({behavior: "smooth"})
  });
 }
 export {femaleClick};
@@ -14,7 +14,7 @@ export {femaleClick};
 let briefClick = () => {
  briefcase.addEventListener('click', (event)=>{
   console.log(event)
-  document.getElementById('experience-typed-text').scrollIntoView()
+  document.getElementById('experience-typed-text').scrollIntoView({behavior: "smooth"})
  });
 }
 export {briefClick};
@@ -22,7 +22,7 @@ export {briefClick};
 let cogsClick = () => {
  cogs.addEventListener('click', (event)=>{
   console.log(event)
-  document.getElementById('skill-typed-text').scrollIntoView()
+  document.getElementById('skill-typed-text').scrollIntoView({behavior: "smooth"})
 
  });
 }
@@ -31,7 +31,7 @@ export {cogsClick};
 let laptopClick = () => {
  laptop.addEventListener('click', (event)=>{
   console.log(event)
-  document.getElementById('project-typed-text').scrollIntoView()
+  document.getElementById('project-typed-text').scrollIntoView({behavior: "smooth"})
  });
 }
 export {laptopClick};
