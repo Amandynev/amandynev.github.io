61b45063e432dc411c7331c9707f0b786e8696ee
Amandyne Verdonck <amandyne.verdonck@gmail.com>
2020-10-12 12:52:41 +0200

    setup i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
   end
 end
--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,8 +1,9 @@
 import Typed from'typed.js';
+import I18n from 'i18n-js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Amandyne Verdonck", "Web developer"],
+    strings: ["Amandyne Verdonck", I18n.t('.banner')],
     typeSpeed: 80,
     loop: true
   });
